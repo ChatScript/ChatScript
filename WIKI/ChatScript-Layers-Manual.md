ab9e08c8c4f8a9b9a72a2809a3ef4b89570a0fc6
chatscriptnlp <gowilcox@gmail.com>
2019-08-19 15:14:02 -0700

    fix from bad merge WIKI

---
+++ WIKI/ChatScript-Layers-Manual.md
@@ -0,0 +1,6 @@
+# ChatScript Layers Manual
+Copyright Bruce Wilcox, gowilcox@gmail.com www.brilligunderstanding.com<br>
+<br>Revision 8/18/2019 cs9.62
+
+
+# LAYERS
--- WIKI/once.bat
+++ WIKI/once.bat
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 
 pandoc ChatScript-JSON.md -s -o ../PDFDOCUMENTATION/ChatScript-JSON.pdf
 pause
-=======
-
-pandoc Practicum-Concepts-and-meaning.md -s -o ../PDFDOCUMENTATION/Practicum-Concepts-and-meaning.pdf
-pause
->>>>>>> f03569f5e633d9a90b489f47b9a3207a7ef4e4f9
